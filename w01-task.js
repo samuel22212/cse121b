b43a2b35199df69dcbea13f6db6dd1f47b7190c4
samuel22212 <samuelporter219@gmail.com>
2024-01-08 11:38:35 -0700

    loop

--- w01-task.js
+++ w01-task.js
@@ -15,6 +15,7 @@ let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
+//this is wrong i believe? 
 function total(...theNumbers) {
 	let sum = 0;
 	for (let aNumber in theNumbers) {
